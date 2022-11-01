3581604b3c5d0e66025b34c3b5d52ef36ebabf8a
Matheus Wenes <matheus.wenes@gmail.com>
2022-11-01 14:49:35 -0300

    java-study2

---
+++ src/TestaCaracteres.java
@@ -0,0 +1,18 @@
+public class TestaCaracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 65;
+        System.out.println(valor);
+
+        valor = (char) (valor + 1);
+        System.out.println(valor);
+
+        String palavra = "Matheus ama alguém: ";
+        System.out.println(palavra);
+
+        palavra = palavra + 2022;
+        System.out.println(palavra);
+    }
+}
