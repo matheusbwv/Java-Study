5b37f3b9d3dceae77344d47a7a1f82d1d8c6f901
Matheus Wenes <matheus.wenes@gmail.com>
2022-11-26 20:18:09 -0300

    java-study

---
+++ src/class-boleano/TestaCondicional2.java
@@ -0,0 +1,17 @@
+import javax.swing.plaf.TreeUI;
+
+public class TestaCondicional2 {
+    public static void main(String[] args) {
+        System.out.println("testando condicionais");
+        int idade = 18;
+        int quantidadePessoas = 3;
+        boolean acompanhado = quantidadePessoas >= 2;
+        System.out.println("Valor de acompanhado é:" + acompanhado);
+        if(idade >= 18 && acompanhado){ 
+            System.out.println("seja bem vindo!!");
+        } else {
+            System.out.println("infelizmente você não pode entrar");
+        }
+    }
+}
+
