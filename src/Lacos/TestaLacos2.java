d3b2ebeae50d3359645443fb06b0475c16b64d11
Matheus Wenes <matheus.wenes@gmail.com>
2022-12-10 12:23:06 -0300

    Java-worked

---
+++ src/Lacos/TestaLacos2.java
@@ -0,0 +1,15 @@
+package Lacos;
+public class TestaLacos2{
+    public static void main(String[] args) {
+        for (int numero = 3; numero <= 100; numero+=3){
+            // if(numero % 3 == 0) {
+                System.out.print(" | ");
+                System.out.print(numero);
+                
+            }
+            // System.out.println();
+        }
+
+        }
+
+
---
+++ src/atividades-alura/LacosDivisores3.java
@@ -0,0 +1,12 @@
+public class LacosDivisores3{
+    public static void main(String[] args) {
+        for (int numero = 3; numero <= 100; numero+=3){
+            // if(numero % 3 == 0) {
+                System.out.print(" | ");
+                System.out.print(numero);
+                
+            }
+            // System.out.println();
+        }
+    }
+
